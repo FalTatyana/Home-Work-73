6b25b65a24438c8ce8bfe5ea8cc073aace716103
Фаль Татьяна <tatty.fal@gmail.com>
2026-07-09 14:39:03 +0600

    Added second task

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 8000;
 
-const PASSWORD = 123;
-
-app.get('/', (req, res) => {
-  res.send('Main Page');
+app.get("/", (req, res) => {
+  res.send("Main Page");
 });
 
-app.get('/:hello', (req, res) => {
-  res.send('Response ' + req.params.hello);
+app.get("/:hello", (req, res) => {
+  res.send("Response " + req.params.hello);
+});
+
+const password = 123;
+const Vigenere = require('caesar-salad').Vigenere;
+
+app.get("/encode/:text", (req, res) => {
+  res.send (Vigenere.Cipher(password).crypt(req.params.text));
+});
+
+app.get("/decode/:text", (req, res) => {
+  res.send(Vigenere.Decipher(password).crypt(req.params.text))
 });
 
 app.listen(port, () => {
-  console.log('Express server listening on port ' + port);
+  console.log("Express server listening on port " + port);
 });
